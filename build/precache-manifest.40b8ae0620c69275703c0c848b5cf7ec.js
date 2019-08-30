6944ad49138d55024744a7092601f7ddb7624d32
wyx8267 <369264448@qq.com>
2019-08-30 20:39:31 +0800

    First Build

---
+++ build/precache-manifest.40b8ae0620c69275703c0c848b5cf7ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61938ac21baa2b2995709e3d4ad0fd4e",
+    "url": "/react-demo-1/build/index.html"
+  },
+  {
+    "revision": "186c529e7f81fdc7ab69",
+    "url": "/react-demo-1/build/static/css/main.a9ea03d4.chunk.css"
+  },
+  {
+    "revision": "245be62aa547326d5714",
+    "url": "/react-demo-1/build/static/js/2.15aef93f.chunk.js"
+  },
+  {
+    "revision": "186c529e7f81fdc7ab69",
+    "url": "/react-demo-1/build/static/js/main.d248ad1f.chunk.js"
+  },
+  {
+    "revision": "e99c4b3a8424d821e678",
+    "url": "/react-demo-1/build/static/js/runtime~main.cdf83837.js"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,71 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+import './style.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Cell = function (props) {
+    return (
+        <div className="cell" onClick={props.onClick}>
+            {props.text}
+        </div>
+    )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Chessboard = function () {
+    const [cells, setCells] = useState([
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+    ])
+    const [finished, setFinished] = useState(false)
+    const [n, setN] = useState(0)
+    const tell = (cells) => {
+        for (let i = 0; i < 3; i++) {
+            if (cells[i][0] === cells[i][1]
+                && cells[i][1] === cells[i][2]
+                && cells[i][2] !== null) {
+                setFinished(true)
+                break
+            }
+        }
+        for (let i = 0; i < 3; i++) {
+            if (cells[0][i] === cells[1][i]
+                && cells[1][i] === cells[2][i]
+                && cells[2][i] !== null) {
+                setFinished(true)
+                break
+            }
+        }
+        if (cells[0][0] === cells[1][1]
+            && cells[1][1] === cells[2][2]
+            && cells[2][2] !== null) {
+            setFinished(true)
+        }
+        if (cells[0][2] === cells[1][1]
+            && cells[1][1] === cells[2][0]
+            && cells[2][0] !== null) {
+            setFinished(true)
+        }
+
+    }
+    const onClickCell = (row, col) => {
+        setN(n + 1)
+        const copy = JSON.parse(JSON.stringify(cells))
+        copy[row][col] = n % 2 === 0 ? 'x' : 'o'
+        setCells(copy)
+        tell(copy)
+    }
+    return (
+        <div className="wrapper">
+            {cells.map((items, row) => <div className="row">
+                {items.map((item, col) => <div className="col">
+                    <Cell text={item} onClick={() => onClickCell(row, col)} />
+                </div>)}
+            </div>)}
+            {finished && <div className="gameover">游戏结束</div>}
+        </div>
+    )
+}
+
+ReactDOM.render(<div>
+    <Chessboard />
+</div>, document.getElementById('root'))
